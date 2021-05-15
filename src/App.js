ac23d6544166749714e318859efe9c8ed858467d
Devang Mathur <masterdevang@gmail.com>
2021-05-15 18:43:37 +0530

    movie component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from "react";
+import Movie from "./Components/Movie";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const FEATURED_API= "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f56668e9c8ca57a5df685488c65eaca5&page=1";
+  const SEARCH_API= "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results)
+      });
+  }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+      fetch(SEARCH_API+searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results)
+      });
+
+     setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+    return (
+      <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+                  <input type="search" placeholder="Search..." className="search"  onChange={handleOnChange} value={searchTerm}/>
+
+        </form>
       </header>
+    <div className="movie-container"> 
+      {movies.length>0 && movies.map((movie) => 
+      <Movie key={movie.id} {...movie}/>
+      )}
+      
     </div>
+    </>
   );
 }
 
